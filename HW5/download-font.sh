8228241a9b6e23cea239fef7919f381e4fb7a631
c4n <udomc.can@gmail.com>
2020-02-17 08:54:05 +0700

    Update lecture 5/homework and demo

--- HW5/download-font.sh
+++
@@ -1,6 +0,0 @@
-wget http://www.pngo.moph.go.th/pngo/phocadownload/DOWNLOAD/Fonts/13ThaiFonts.zip
-unzip 13ThaiFonts.zip 
-cd 13ThaiFonts/
-sudo cp * /usr/share/fonts/truetype
-fc-cache -f -v
-rm -fr ~/.cache/matplotlib
